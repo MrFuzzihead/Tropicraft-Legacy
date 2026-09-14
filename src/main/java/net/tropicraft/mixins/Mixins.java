d8ab4cc2350c285f5011e50c9aa9a5ce17c32125
Duncan Campbell <campbellduncan12@gmail.com>
2026-09-14 13:18:29 -0500

    Mixin boilerplate

---
+++ src/main/java/net/tropicraft/mixins/Mixins.java
@@ -0,0 +1,24 @@
+package net.tropicraft.mixins;
+
+import javax.annotation.Nonnull;
+
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+import com.gtnewhorizon.gtnhmixins.builders.MixinBuilder;
+
+public enum Mixins implements IMixins {
+
+    MINECRAFT(new MixinBuilder().setPhase(Phase.EARLY)
+        .addCommonMixins("EXAMPLECLASS"));
+
+    private final MixinBuilder builder;
+
+    Mixins(MixinBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Nonnull
+    @Override
+    public MixinBuilder getBuilder() {
+        return builder;
+    }
+}
