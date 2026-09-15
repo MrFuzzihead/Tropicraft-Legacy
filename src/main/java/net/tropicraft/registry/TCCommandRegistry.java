49b717ed86fc2d81a2f406e46b7bf6e39450f0ea
Duncan Campbell <campbellduncan12@gmail.com>
2026-09-15 10:38:30 -0500

    Fix the comment above the /tc registration, /tropics is gone

--- src/main/java/net/tropicraft/registry/TCCommandRegistry.java
+++ src/main/java/net/tropicraft/registry/TCCommandRegistry.java
@@ -8,9 +8,7 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 public class TCCommandRegistry {
 
     public static void init(final FMLServerStartingEvent event) {
-        // /tc is the root for everything the mod adds - /tc time, /tc weather, /tc tropics. The only
-        // command registered on its own is /tropics, which players have been typing since long before
-        // there was a root to put it under.
+        // /tc is the only root the mod registers - /tc time, /tc weather and /tc tropics hang off it.
         event.registerServerCommand((ICommand) new CommandTropicraft());
     }
 }
